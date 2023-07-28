2dfa4d6bc8dcff49701738300cebd4301ea5adb4
JakobVogt121 <jakob.vogt@gmx.net>
2023-07-28 17:21:58 +0200

    added easyGate/Valves
    Node-Red<-Seriell->easyGate<-LoRa->easyValves

---
+++ Easy/easyGate/easyGate.ino
@@ -0,0 +1,89 @@
+/*==========================================================================================
+                                          easyGate
+  ==========================================================================================*/
+
+/* Description
+Erhält von Node-Red über Serielle Schnittstelle:
+  10: Ventil 1 aus
+  11: Ventil 1 an
+  20: Ventil 1 aus
+  21: Ventil 1 an
+und sendet die Daten über LoRa weiter an easyValves.
+Bekommt Daten von easyValves über LoRa zurück und sendet diese wieder über Serielle Schnittstelle an Node-Red
+  10: Ventil 1 aus
+  11: Ventil 1 an
+  20: Ventil 1 aus
+  21: Ventil 1 an
+*/
+
+#include <LoRa.h>
+
+// Lora Pin Settings
+#define ss 18
+#define rst 14
+#define dio0 26
+
+const byte gateAddress = 0;
+const byte valve1Address = 1;
+const byte valve2Address = 2;
+byte receiverAddress = 0;
+byte state;
+
+void setup() {
+  // Serial Communication (debugging)
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+  // LoRa Communication
+  SPI.begin(5, 19, 27, 18);
+  LoRa.setPins(ss, rst, dio0);
+  if (!LoRa.begin(868E6)) {
+    Serial.println("Start of LoRa failed!");
+    while (1)
+      ;
+  }
+  delay(1000);
+  pinMode(LED_BUILTIN, OUTPUT);  // for debugging/visualization
+  Serial.println("Gate ready");
+}
+
+void loop() {
+  while (Serial.available()) {
+    delay(2);  //delay to allow byte to arrive in input buffer
+    char c_receiverAddress = Serial.read();
+    receiverAddress = c_receiverAddress - '0';
+    char c_state = Serial.read();
+    state = c_state - '0';
+    delay(10);
+    Serial.println("Gate sends: ");
+    Serial.print("Receiver Valve: ");
+    Serial.print(receiverAddress);
+    Serial.print("\nstate: ");
+    Serial.println(state);
+
+    LoRa.beginPacket();
+    LoRa.write(receiverAddress);
+    LoRa.write(state);
+    LoRa.endPacket();
+  }
+
+  onLoRaonReceive(LoRa.parsePacket());
+}
+
+void onLoRaonReceive(int packetSize) {
+  //digitalWrite(LED_BUILTIN, HIGH);
+  if (packetSize == 0) return;  // if there's no packet, return
+
+  int target = LoRa.read();
+  int state = LoRa.read();
+
+  Serial.println("Received from Valves: ");
+  Serial.print(target);
+  Serial.print(state);
+  Serial.println();
+
+  //Serial.print("RSSI: ");
+  //Serial.println(LoRa.packetRssi());
+  //delay(1000);
+  //digitalWrite(LED_BUILTIN, LOW);
+}
---
+++ Easy/easyValves/easyValves.ino
@@ -0,0 +1,92 @@
+/*==========================================================================================
+                                          easyValves
+  ==========================================================================================*/
+  
+/* Description
+Erhält Daten über LoRa von easyGate:
+  10: Ventil 1 aus
+  11: Ventil 1 an
+  20: Ventil 1 aus
+  21: Ventil 1 an
+und sendet Bestätigung über LoRa an easyGate zurück:
+  10: Ventil 1 aus
+  11: Ventil 1 an
+  20: Ventil 1 aus
+  21: Ventil 1 an
+*/
+
+#include <LoRa.h>
+
+#define ss 18
+#define rst 14
+#define dio0 26
+
+#define pinV1 32
+#define pinV2 33
+
+byte gateAddress = 0;
+byte valve1Address = 1;
+byte valve2Address = 2;
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);  // for debugging/visualization
+  pinMode(pinV1, OUTPUT);
+  pinMode(pinV2, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial)
+    ;
+  SPI.begin(5, 19, 27, 18);
+  LoRa.setPins(ss, rst, dio0);
+  if (!LoRa.begin(868E6)) {
+    Serial.println("Start of LoRa failed!");
+    while (1)
+      ;
+  }
+  delay(1000);
+  Serial.println("Valves ready");
+}
+
+void loop() {
+  onLoRaReceive(LoRa.parsePacket());
+}
+
+void onLoRaReceive(int packetSize) {
+  //digitalWrite(LED_BUILTIN, HIGH);
+  if (packetSize == 0) return;  // if there's no packet, return
+
+  byte target = LoRa.read();
+  byte state = LoRa.read();
+
+  Serial.println("Target: ");
+  Serial.println(target);
+  Serial.println("State: ");
+  Serial.println(state);
+
+  if (target == valve1Address) {
+    Serial.println("V1");
+    digitalWrite(pinV1, state);
+    confirm_cmd(target, state);
+  } else if (target == valve2Address) {
+    Serial.println("V2");
+    digitalWrite(pinV2, state);
+    confirm_cmd(target, state);
+  } else {
+    Serial.println("Error");
+    return;  // skip rest of function
+  }
+  //delay(1000);
+  //digitalWrite(LED_BUILTIN, LOW);
+  //Serial.print("RSSI: ");
+  //Serial.println(LoRa.packetRssi());
+}
+
+void confirm_cmd(byte target, byte state) {
+  delay(1000);
+  Serial.print(target);
+  Serial.print(state);
+  Serial.println();
+  LoRa.beginPacket();
+  LoRa.write(target);
+  LoRa.write(state);
+  LoRa.endPacket();
+}
