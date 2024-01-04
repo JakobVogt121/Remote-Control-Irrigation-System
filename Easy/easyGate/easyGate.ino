4cf11d833d499763515371629d2cba96a8e1d67f
JakobVogt121 <80894564+JakobVogt121@users.noreply.github.com>
2024-01-04 23:16:58 +0100

    Revert "corrected comments"
    This reverts commit 89f8a0b8a015e4fdbb6d900a346f3473c46ebf6d.

--- Easy/easyGate/easyGate.ino
+++ Easy/easyGate/easyGate.ino
@@ -6,14 +6,14 @@
 Erhält von Node-Red über Serielle Schnittstelle:
   10: Ventil 1 aus
   11: Ventil 1 an
-  20: Ventil 2 aus
-  21: Ventil 2 an
+  20: Ventil 1 aus
+  21: Ventil 1 an
 und sendet die Daten über LoRa weiter an easyValves.
 Bekommt Daten von easyValves über LoRa zurück und sendet diese wieder über Serielle Schnittstelle an Node-Red
   10: Ventil 1 aus
   11: Ventil 1 an
-  20: Ventil 2 aus
-  21: Ventil 2 an
+  20: Ventil 1 aus
+  21: Ventil 1 an
 */
 
 #include <LoRa.h>
@@ -26,7 +26,6 @@ Bekommt Daten von easyValves über LoRa zurück und sendet diese wieder über Se
 const byte gateAddress = 0;
 const byte valve1Address = 1;
 const byte valve2Address = 2;
-const byte valve2Address = 3;
 byte receiverAddress = 0;
 byte state;
 
@@ -68,12 +67,12 @@ void loop() {
     LoRa.endPacket();
   }
 
-  onLoRaReceive(LoRa.parsePacket());
+  onLoRaonReceive(LoRa.parsePacket());
 }
 
-void onLoRaReceive(int packetSize) {
+void onLoRaonReceive(int packetSize) {
   if (packetSize == 2) {
-    // gateAddress not used to check if message is for gate
+
     int target = LoRa.read();
     int state = LoRa.read();
 
